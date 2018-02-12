7940f5095221ec0f744e6d58b2a0f42dfc96fcf8
swizard12 <35807828+swizard12@users.noreply.github.com>
2018-02-12 17:04:11 +0000

    Filter Boxes for SW and EMP Lists

--- WiTrainingSuite/WiTrainingSuite/ViewModel/EmployeeMasterViewModel.cs
+++ WiTrainingSuite/WiTrainingSuite/ViewModel/EmployeeMasterViewModel.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Linq.Dynamic;
 using System.Reactive.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using WiTrainingSuite.Model;
 
@@ -43,6 +44,7 @@ namespace WiTrainingSuite.ViewModel
             using (Wi_training_suite db = new Wi_training_suite(App.ConString))
             {
                 EmployeeList = new ReactiveList<FnTEMPLOYEE_LISTResult>(db.FnTEMPLOYEE_LIST().OrderBy("EMP_LNAME"));
+                OriginalList = EmployeeList;
             }
             SnackBarQueue.Enqueue("Load Successful");
 
@@ -50,10 +52,7 @@ namespace WiTrainingSuite.ViewModel
             {
                 IsBtmOpen = false;
 
-                string f = BuildFilterString(SortList);
-
-                if (!string.IsNullOrWhiteSpace(f) && EmployeeList != null)
-                    EmployeeList = new ReactiveList<FnTEMPLOYEE_LISTResult>(EmployeeList.OrderBy(f, this).AsEnumerable());
+                ApplySort();
             });
 
             NewEmployeeCommand = ReactiveCommand.Create(() =>
@@ -86,6 +85,48 @@ namespace WiTrainingSuite.ViewModel
             ColumnHeaders.Add("ROLE_NAME", "Role");
             ColumnHeaders.Add("VAR_NAME", "Variant");
             ColumnHeaders.Add("SHIFT_NAME", "Shift");
+
+            ClearNameFilter = ReactiveCommand.Create(() =>
+            {
+                fNAME = string.Empty;
+            }, this.WhenAny(
+                x => x.fNAME,
+                (y) => !string.IsNullOrWhiteSpace(y.Value)));
+
+            ClearDeptFilter = ReactiveCommand.Create(() =>
+            {
+                fDEPT = string.Empty;
+            }, this.WhenAny(
+                x => x.fDEPT,
+                (y) => !string.IsNullOrWhiteSpace(y.Value)));
+
+            ClearRoleFilter = ReactiveCommand.Create(() =>
+            {
+                fROLE = string.Empty;
+            }, this.WhenAny(
+                x => x.fROLE,
+                (y) => !string.IsNullOrWhiteSpace(y.Value)));
+
+            ClearAll = ReactiveCommand.Create(() =>
+            {
+                fNAME = string.Empty;
+                fDEPT = string.Empty;
+                fROLE = string.Empty;
+            }, this.WhenAny(
+                x => x.fNAME,
+                x => x.fDEPT,
+                x => x.fROLE,
+                (n, d, r) => !string.IsNullOrWhiteSpace(n.Value) || 
+                             !string.IsNullOrWhiteSpace(d.Value) || 
+                             !string.IsNullOrWhiteSpace(r.Value)));
+        }
+
+        public void ApplySort()
+        {
+            string f = BuildFilterString(SortList);
+
+            if (!string.IsNullOrWhiteSpace(f) && EmployeeList != null)
+                EmployeeList = new ReactiveList<FnTEMPLOYEE_LISTResult>(EmployeeList.OrderBy(f, this).AsEnumerable());
         }
 
         private int _EmployeeIndex = -1;
@@ -99,6 +140,18 @@ namespace WiTrainingSuite.ViewModel
         public ReactiveCommand EditEmployeeCommand { get; set; }
         public ReactiveCommand EditTrainingCommand { get; set; }
 
+        public ReactiveCommand ClearNameFilter { get; set; }
+        public ReactiveCommand ClearDeptFilter { get; set; }
+        public ReactiveCommand ClearRoleFilter { get; set; }
+        public ReactiveCommand ClearAll { get; set; }
+
+        private ReactiveList<FnTEMPLOYEE_LISTResult> _OriginalList = new ReactiveList<FnTEMPLOYEE_LISTResult>();
+        public ReactiveList<FnTEMPLOYEE_LISTResult> OriginalList
+        {
+            get { return _OriginalList; }
+            set { this.RaiseAndSetIfChanged(ref _OriginalList, value); }
+        }
+
         private ReactiveList<FnTEMPLOYEE_LISTResult> _employeeList;
         public ReactiveList<FnTEMPLOYEE_LISTResult> EmployeeList
         {
@@ -111,6 +164,82 @@ namespace WiTrainingSuite.ViewModel
         {
             get { return _selectedEmployee; }
             set { this.RaiseAndSetIfChanged(ref _selectedEmployee, value); }
+        }
+
+        private string _fNAME;
+        public string fNAME
+        {
+            get { return _fNAME; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _fNAME, value);
+
+                var task = Task.Run(() => ApplyFilter());
+                bWorking = true;
+                task.ContinueWith((x) => { bWorking = false; });
+            }
+        }
+
+        private string _fDEPT;
+        public string fDEPT
+        {
+            get { return _fDEPT; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _fDEPT, value);
+
+                var task = Task.Run(() => ApplyFilter());
+                bWorking = true;
+                task.ContinueWith((x) => { bWorking = false; });
+            }
+        }
+
+        private string _fROLE;
+        public string fROLE
+        {
+            get { return _fROLE; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _fROLE, value);
+
+                var task = Task.Run(() => ApplyFilter());
+                bWorking = true;
+                task.ContinueWith((x) => { bWorking = false; });
+            }
+        }
+
+        private async void ApplyFilter()
+        {
+            await Task.Run(() => 
+            {
+                var tList = new ReactiveList<FnTEMPLOYEE_LISTResult>(OriginalList);
+
+                // Pass One - First Name / Last Name
+                if (!string.IsNullOrWhiteSpace(fNAME)) { tList = new ReactiveList<FnTEMPLOYEE_LISTResult>(OriginalList.Where(x => x.EMP_FNAME.ToUpper().Contains(fNAME.ToUpper()) || x.EMP_LNAME.ToUpper().Contains(fNAME.ToUpper()))); }
+                else { tList = OriginalList; }
+
+                // Pass Two - Department
+                if (!string.IsNullOrWhiteSpace(fDEPT)) { tList = new ReactiveList<FnTEMPLOYEE_LISTResult>(tList.Where(x => x.DEPT_NAME.ToUpper().Contains(fDEPT.ToUpper()))); }
+
+                // Pass Three - Role / Var
+                if (!string.IsNullOrEmpty(fROLE)) { tList = new ReactiveList<FnTEMPLOYEE_LISTResult>(tList.Where(x => x.ROLE_NAME.ToUpper().Contains(fROLE.ToUpper()) || x.VAR_NAME.ToUpper().Contains(fROLE.ToUpper()))); }
+
+                EmployeeList = tList;
+
+                ApplySort();
+
+                EmployeeIndex = -1;
+
+                return 0;
+            });
+        }
+
+
+        private bool _bWorking = false;
+        public bool bWorking
+        {
+            get { return _bWorking; }
+            set { this.RaiseAndSetIfChanged(ref _bWorking, value); }
         }
     }
 }
--- WiTrainingSuite/WiTrainingSuite/ViewModel/StandardWorkMasterViewModel.cs
+++ WiTrainingSuite/WiTrainingSuite/ViewModel/StandardWorkMasterViewModel.cs
@@ -47,6 +47,7 @@ namespace WiTrainingSuite.ViewModel
             using (Wi_training_suite db = new Wi_training_suite(App.ConString))
             {
                 StandardWorkList = new ReactiveList<FnTSTANDARDWORK_LISTResult>(db.FnTSTANDARDWORK_LIST().OrderBy("SW_CODE", this).AsEnumerable());
+                OriginalList = StandardWorkList;
             }
             SnackBarQueue.Enqueue("Load Successful");
 
@@ -72,8 +73,32 @@ namespace WiTrainingSuite.ViewModel
                 HostScreen.Router.Navigate.Execute(new StandardWorkDetailEditViewModel(HostScreen, SelectedStandardWork));
             }, this.WhenAny(
                 x => x.StandardWorkIndex,
-                x => x.IsAdmin, 
+                x => x.IsAdmin,
                 (i, a) => i.Value > -1 && a.Value == true));
+
+            ClearCodeFilter = ReactiveCommand.Create(() =>
+            {
+                fCODE = string.Empty;
+            }, this.WhenAny(
+                x => x.fCODE,
+                (y) => !string.IsNullOrWhiteSpace(y.Value)));
+
+            ClearDescFilter = ReactiveCommand.Create(() =>
+            {
+                fDESC = string.Empty;
+            }, this.WhenAny(
+                x => x.fDESC,
+                (y) => !string.IsNullOrWhiteSpace(y.Value)));
+
+            ClearAll = ReactiveCommand.Create(() =>
+            {
+                fCODE = string.Empty;
+                fDESC = string.Empty;
+            }, this.WhenAny(
+                x => x.fCODE,
+                x => x.fDESC,
+                (c, d) => !string.IsNullOrWhiteSpace(c.Value) ||
+                          !string.IsNullOrWhiteSpace(d.Value)));
 
             // Key = SQL Column Header; Value = Friendly Name
             ColumnHeaders.Add("SW_CODE", "Code");
@@ -81,6 +106,14 @@ namespace WiTrainingSuite.ViewModel
             ColumnHeaders.Add("SW_ISSUE", "Issue #");
             ColumnHeaders.Add("SW_ISSUEDATE", "Issue Date");
             ColumnHeaders.Add("SW_RA", "Risk Assessment #");
+        }
+
+        public void ApplySort()
+        {
+            string f = BuildFilterString(SortList);
+
+            if (!string.IsNullOrWhiteSpace(f) && StandardWorkList != null)
+                StandardWorkList = new ReactiveList<FnTSTANDARDWORK_LISTResult>(StandardWorkList.OrderBy(f, this).AsEnumerable());
         }
 
         private int _StandardWorkIndex = -1;
@@ -92,6 +125,42 @@ namespace WiTrainingSuite.ViewModel
 
         public ReactiveCommand NewStandardWorkCommand { get; set; }
         public ReactiveCommand EditStandardWorkCommand { get; set; }
+        public ReactiveCommand ClearCodeFilter { get; set; }
+        public ReactiveCommand ClearDescFilter { get; set; }
+        public ReactiveCommand ClearAll { get; set; }
+
+        private bool _bWorking = false;
+        public bool bWorking
+        {
+            get { return _bWorking; }
+            set { this.RaiseAndSetIfChanged(ref _bWorking, value); }
+        }
+
+        private string _fCODE;
+        public string fCODE
+        {
+            get { return _fCODE; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _fCODE, value);
+                var task = Task.Run(() => ApplyFilter());
+                bWorking = true;
+                task.ContinueWith((x) => { bWorking = false; });
+            }
+        }
+
+        private string _fDESC;
+        public string fDESC
+        {
+            get { return _fDESC; }
+            set
+            {
+                this.RaiseAndSetIfChanged(ref _fDESC, value);
+                var task = Task.Run(() => ApplyFilter());
+                bWorking = true;
+                task.ContinueWith((x) => { bWorking = false; });
+            }
+        }
 
         private ReactiveList<FnTSTANDARDWORK_LISTResult> _StandardWorkList;
         public ReactiveList<FnTSTANDARDWORK_LISTResult> StandardWorkList
@@ -105,6 +174,36 @@ namespace WiTrainingSuite.ViewModel
         {
             get { return _SelectedStandardWork; }
             set { this.RaiseAndSetIfChanged(ref _SelectedStandardWork, value); }
+        }
+
+        private ReactiveList<FnTSTANDARDWORK_LISTResult> _OriginalList = new ReactiveList<FnTSTANDARDWORK_LISTResult>();
+        public ReactiveList<FnTSTANDARDWORK_LISTResult> OriginalList
+        {
+            get { return _OriginalList; }
+            set { this.RaiseAndSetIfChanged(ref _OriginalList, value); }
+        }
+
+        private async void ApplyFilter()
+        {
+            await Task.Run(() =>
+            {
+                var tList = new ReactiveList<FnTSTANDARDWORK_LISTResult>(OriginalList);
+
+                // Pass One - SW Code
+                if (!string.IsNullOrWhiteSpace(fCODE)) { tList = new ReactiveList<FnTSTANDARDWORK_LISTResult>(OriginalList.Where(x => x.SW_CODE.ToUpper().Contains(fCODE.ToUpper()))); }
+                else { tList = OriginalList; }
+
+                // Pass Two - Description
+                if (!string.IsNullOrWhiteSpace(fDESC)) { tList = new ReactiveList<FnTSTANDARDWORK_LISTResult>(tList.Where(x => x.SW_DESCRIPTION.ToUpper().Contains(fCODE.ToUpper()))); }
+
+                StandardWorkList = tList;
+
+                ApplySort();
+
+                StandardWorkIndex = -1;
+
+                return 0;
+            });
         }
 
         #endregion
